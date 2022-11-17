acada17f6140515bb6a0c73052c2d5e9a1bd4402
root <root@BA00058.Bioaster.local>
2022-11-17 17:06:54 +0100

    tatakae !

---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+mod utils;
+
+use wasm_bindgen::prelude::*;
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greetings(name: &str, nb: u32) {
+    alert(&format!("Hello, {}! and the number is {}", name, nb));
+}
