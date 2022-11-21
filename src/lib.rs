cb0c2458b6f03cc5592d0e196247d28ecb6b3e08
root <root@BA00058.Bioaster.local>
2022-11-21 17:32:15 +0100

    window

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,32 @@ extern "C" {
     fn alert(s: &str);
 }
 
-#[wasm_bindgen]
-pub fn greetings(name: &str, nb: u32) {
-    alert(&format!("Hello, {}! and the number is {}", name, nb));
+// #[wasm_bindgen]
+// pub fn greetings(name: &str, nb: u32) {
+//     alert(&format!("Hello, {}! and the number is {}", name, nb));
+// }
+
+// #[wasm_bindgen]
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Cell {
+    Dead = 0,
+    Alive = 1,
+}
+
+pub struct Window {
+    width: u32,
+    height: u32,
+    cells: Vec<Cell>,
+}
+
+impl Window {
+    fn _comp_idx(&self, row: u32, col: u32) -> usize {
+        (row * self.width + col) as usize
+    }
+
+    fn at(&self, row: u32, col: u32) -> &Cell {
+        let idx = self._comp_idx(row, col);
+        &self.cells[idx]
+    }
 }
